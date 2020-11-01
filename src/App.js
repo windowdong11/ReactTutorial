65952e274be199daba6db2d3c22d213f8acd8d97
WonDongGyu <windowdong11@naver.com>
2020-11-02 00:41:27 +0900

    react-router-dom을 이용한 SPA
    좋아요 버튼, 카운터, 히스토리를 나눔

--- src/App.js
+++ src/App.js
@@ -4,28 +4,33 @@ import './App.css';
 import LikeButton from './pages/LikeButton';
 import Container from './pages/Container';
 import History from './pages/History';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+const BodyWrapper = styled.div`
+  padding: 20;
+  border: 5px solid gray;
+`
 
 function App() {
   console.log(process.env)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <LikeButton></LikeButton>
-        <Container></Container>
-        <History></History>
       서버 : {process.env.REACT_APP_SERVER}
+        <BrowserRouter>
+          <BodyWrapper>
+            <Link to='/likeButton'>좋아요 버튼</Link>
+            <br/>
+            <Link to='/container'>컨테이너</Link>
+            <br/>
+            <Link to='/history'>히스토리</Link>
+            <br/>
+            <Route path='/likeButton' component={LikeButton}></Route>
+            <Route path='/container' component={Container}></Route>
+            <Route path="/history" component={History}></Route>
+          </BodyWrapper>
+        </BrowserRouter>
       </header>
     </div>
   );
