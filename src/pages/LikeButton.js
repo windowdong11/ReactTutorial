97c61f02dc62a986f327d256d7e705b1521274dd
WonDongGyu <windowdong11@naver.com>
2020-11-02 00:28:53 +0900

    리팩터링 : LikeButton을 styled-component를 사용하여 작성]

--- src/pages/LikeButton.js
+++ src/pages/LikeButton.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+    background: ${props => (props.liked ? "gray" : "red")};
+`
 
 export default function LikeButton() {
     const [liked, setLiked] = useState(false)
     const text = liked ? "싫어요" : "좋아요"
-    const props = {
-        onClick: () => setLiked(!liked),
-        style: {
-            backgroundColor: liked ? "gray" : "red"
-        }
-    }
-    return <button {...props}>{text}</button>
+    return <StyledButton liked={liked} onClick={() => setLiked(!liked)}>{text}</StyledButton>
 }
